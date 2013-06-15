8c93744db6c5dd709e57dd67f45663ac2e5e33d2
Vanessa Suárez <vane.sb@gmail.com>
2013-06-15 14:36:29 -0430

    Creación del proyecto y sus clases
    Se creó el proyecto completo y se crearon las clases, aún sin herencia
    ni atributos ni nada

---
+++ PracticaGitHub/src/PracticaGitHub/Obra.java
@@ -0,0 +1,16 @@
+/*
+  * ----PRACTICA GITHUB----
+  * -------Autores-------
+  * Angarita, Lakhsmi
+  * Ramírez, Gerson
+  * Suárez, Vanessa
+ */
+package PracticaGitHub;
+
+/**
+ *
+ * @author Vanessa
+ */
+public class Obra {
+    
+}
---
+++ PracticaGitHub/src/PracticaGitHub/Principal.java
@@ -0,0 +1,18 @@
+/*
+  * ----PRACTICA GITHUB----
+  * -------Autores-------
+  * Angarita, Lakhsmi
+  * Ramírez, Gerson
+  * Suárez, Vanessa
+ */
+package PracticaGitHub;
+
+/**
+ *
+ * @author Vanessa
+ */
+public class Principal {
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
